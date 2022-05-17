e3122b12bde8eda0edac12ab9048131e52030457
Benatovich <sambenatovich@gmail.com>
2022-05-17 19:28:51 -0400

    basic file structure set up

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddItem from '../components/AddItem'
+import List from '../components/List'
+
+export default function Home() {
+  return (
+    <main>
+        <AddItem />
+        <List />
+    </main>
+  )
+}

813890ce470a12a78797bd04b7df61eeb92eab9a
Benatovich <sambenatovich@gmail.com>
2022-05-20 18:32:38 -0400

    bad news: edit is having the same problem as copy. good news: i bet it has the same solution.

--- src/views/Builds/EditBuild.jsx
+++ src/views/Builds/EditBuild.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { Link, useParams, useHistory } from 'react-router-dom';
+import { useBuild } from '../../hooks/builds';
+import { useAuth } from '../../hooks/user';
+import BuildForm from '../../components/Builds/BuildForm';
 
 export default function EditBuild() {
+  const history = useHistory();
+  const { id } = useParams();
+  const { build, update } = useBuild(id);
+  const { user } = useAuth();
+
+  if(!build) return null;
+
+  const isOwner = user.id === build.userId;
+  const detailUrl = `/builds/${id}`;
+
+  if(!isOwner) {
+    history.replace(detailUrl);
+    return null;
+  }
+
+  const handleSubmit = async (edited) => {
+    await update(edited);
+    history.push('/builds');
+  };
+  
   return (
-    <div>EditBuild</div>
+    <div>
+      <div>
+        <Link to='/builds'>
+          Return to build list
+        </Link>
+        {' / '}
+        <Link to={detailUrl}>
+          {build.name}
+        </Link>
+      </div>
+
+      <BuildForm
+        label='Edit Build'
+        build={build}
+        onSubmit={handleSubmit}
+      />
+    </div>
   )
 }
